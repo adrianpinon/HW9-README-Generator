2b8f79855a52710693030b72505e8b3d6622351e
Adrian <70490120+adrianpinon@users.noreply.github.com>
2020-12-03 15:53:20 -0600

    added starter code and a little bit of my own to initial

---
+++ index.js
@@ -0,0 +1,37 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown")
+    console.log(generateMarkdown)
+
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        message: "Provide a description of your application",
+        name: "description",
+    },
+    {
+        type: "list",
+        message: "What license do you want include in your application?",
+        name: "license",
+        choices: ["MIT", "GPU"]
+    }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data),function(err){
+        if (err) throw err
+    })
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then(function(answers) {
+        console.log(answers)
+    })
+}
+
+// function call to initialize program
+init();
